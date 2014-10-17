1bce4db06734d83550b543ad14f47f12d08a3f12
KrasimirVasilev <krasimirsv@abv.bg>
2014-10-17 17:21:08 +0300

    First task

--- app/src/main/java/tutorials/hackbulgaria/com/week0_widgets/MainActivity.java
+++ app/src/main/java/tutorials/hackbulgaria/com/week0_widgets/MainActivity.java
@@ -1,19 +1,58 @@
 package tutorials.hackbulgaria.com.week0_widgets;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private View coloredView;
+    private EditText hexCodeEditText;
+    private Button showColorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coloredView = findViewById(R.id.coloredView);
+        hexCodeEditText = (EditText) findViewById(R.id.hexCodeEditText);
+        showColorButton = (Button) findViewById(R.id.showColorButton);
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        showColorButton.setOnClickListener(this);
+        hexCodeEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                changeColor(editable.toString());
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,5 +71,53 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.showColorButton) {
+
+            String hexCode = hexCodeEditText.getText().toString().trim();
+
+            if (!isHexCodeEmpty(hexCode)) {
+
+                changeColor(hexCode);
+            }
+        }
+    }
+
+    private void changeColor(String hexCode) {
+
+        if (hexCode != null) {
+
+            hexCodeEditText.setSelection(hexCode.length());
+
+            if (!hexCode.startsWith("#")) {
+                hexCode = "#" + hexCode;
+                hexCodeEditText.setText(hexCode);
+            }
+
+            try {
+
+                coloredView.setBackgroundColor(Color.parseColor(hexCode));
+
+            } catch (IllegalArgumentException ex) {
+                //Do Nothing
+            }
+        }
+    }
+
+    private boolean isHexCodeEmpty(String hexCode) {
+
+        if (hexCode != null) {
+
+            if (!hexCode.equals(""))
+                return false;
+        }
+
+        Toast.makeText(this, getString(R.string.hex_code_required_message), Toast.LENGTH_SHORT).show();
+
+        return true;
     }
 }
